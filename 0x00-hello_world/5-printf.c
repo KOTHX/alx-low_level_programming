3866f94db6b53e25aa7c3c0b58046c8d82aad4ea
KOTH <karim.othman6@gmail.com>
2024-05-14 10:28:20 +0000

    fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  * main - Entry point
  * Description: A Printf program -Wall gcc
  * Return: Always 0 (Success)
 */
-int main(void){
+int main(void)
+{
 printf("\"with proper grammar, but the outcome is a piece of art,\n");
 return(0);
 }
